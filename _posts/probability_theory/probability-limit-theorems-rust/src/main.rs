bbe71e1fc5a6700c7c17295dcf2eef80fda4e69b
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-02-15 15:35:11 -0500

    rust update

--- _posts/probability_theory/probability-limit-theorems-rust/src/main.rs
+++ _posts/probability_theory/probability-limit-theorems-rust/src/main.rs
@@ -6,14 +6,16 @@ use rand::prelude::*;
 fn main() {
     //let mut rng = ChaCha8Rng::seed_from_u64(314159);
     let mut rng = thread_rng();
-    let n: usize = 10_000;
+    let n: usize = 1000;
 
     let xs = get_xs(n, &mut rng);
     let xbars = get_xbars(&xs);
+    let llns = get_llns(&xbars);
 
-    //println!("{:?}", xs);
-    //println!("{:?}", xbars);
-    println!("{:?}", xbars[xbars.len() - 1]);
+    println!("{:?}", xs);
+    println!("{:?}", xbars);
+    println!("{:?}", llns);
+    //println!("{:?}", xbars[xbars.len() - 1]);
 }
 
 fn get_xs(n: usize, rng: &mut ThreadRng) -> Vec<f64> {
@@ -31,11 +33,23 @@ fn get_xbars(xs: &Vec<f64>) -> Vec<f64> {
 
     let n = xs.len();
     let mut xbars = vec![0f64; n];
-    xbars[0] = xs[0] as f64;
+    xbars[0] = xs[0];
 
     for i in 1..n {
-        xbars[i] = (i as f64 * xbars[i - 1] + xs[i] as f64) / (i + 1) as f64;
+        xbars[i] = (i as f64 * xbars[i - 1] + xs[i]) / (i + 1) as f64;
     }
 
     return xbars;
+}
+
+fn get_llns(xbars: &Vec<f64>) -> Vec<f64> {
+
+    let n = xbars.len();
+    let mut llns = vec![0f64; n];
+
+    for i in 1..n {
+        llns[i] = xbars[i] - 0.5
+    }
+
+    return llns;
 }
