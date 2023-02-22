1c72516c9ca0a22daced42e8d529d00ca2f13ebb
William G Underwood <42812654+WGUNDERWOOD@users.noreply.github.com>
2023-02-22 13:56:31 -0500

    started gaussian chaining post

---
+++ _posts/gaussian_chaining/2023-02-20-gaussian-chaining.markdown
@@ -0,0 +1,36 @@
+---
+layout: post
+title:  "Sub-Gaussian Chaining and Dudley's Theorem"
+date:   2023-02-21
+---
+
+TODO Abstract
+
+## Introduction
+
+TODO Controlling sup of stochastic processes is important -- examples
+TODO Define subg process
+
+## Covering numbers
+
+TODO Define and give properties
+
+## Dudley's theorem
+
+TODO State result
+
+## Sub-Gaussian chaining
+
+TODO Use the chaining method to prove Dudley's theorem
+
+## Examples
+
+### Example 1: radial basis function kernel
+
+TODO Figure: sample trajectories from RBF GP with a different scales
+TODO Figure: Empirical expected supremum and Dudley's bound
+
+### Example 2: Ornstein--Uhlenbeck kernel
+
+TODO Figure: sample trajectories from OU GP with a different scales
+TODO Figure: Empirical expected supremum and Dudley's bound
--- _posts/gaussian_chaining/gaussian_chaining.jl
+++ _posts/gaussian_chaining/gaussian_chaining.jl
@@ -1,7 +1,8 @@
 using Distributions
 using LinearAlgebra
 using Random
-using UnicodePlots
+using PyPlot
+
 
 struct Interval
     lo::Float64
@@ -11,15 +12,19 @@ end
 abstract type Kernel end
 
 struct RBFKernel <: Kernel
-    scale::Float64
+    l::Float64
 end
 
 struct OUKernel <: Kernel
-    scale::Float64
+    l::Float64
 end
 
-evaluate(x::Float64, y::Float64, kernel::OUKernel) = exp(-abs(x-y) / kernel.scale)
-evaluate(x::Float64, y::Float64, kernel::RBFKernel) = exp(-(x-y)^2 / (2*kernel.scale^2))
+evaluate(x::Float64, y::Float64, kernel::OUKernel) = exp(-abs(x-y) / kernel.l)
+evaluate(x::Float64, y::Float64, kernel::RBFKernel) = exp(-(x-y)^2 / (2*kernel.l^2))
+name(kernel::OUKernel) = "Ornstein--Uhlenbeck"
+name(kernel::RBFKernel) = "Radial basis function"
+shortname(kernel::OUKernel) = "OU"
+shortname(kernel::RBFKernel) = "RBF"
 
 function sample_gaussian(Sigma_svd::SVD)
     Z = rand(Normal(0, 1), length(Sigma_svd.S))
@@ -30,16 +35,69 @@ function get_Sigma(xs::Vector{Float64}, kernel::Kernel)
     return broadcast((x, y) -> evaluate(x, y, kernel), xs, xs')
 end
 
-scale = 0.3
-#kernel = OUKernel(scale)
-kernel = RBFKernel(scale)
-ngrid = 1000
-xs = collect(range(0, 1, length = ngrid))
-Sigma = get_Sigma(xs, kernel)
-Sigma_svd = svd(Sigma)
-Z = sample_gaussian(Sigma_svd)
+function format_plot(fig, ax)
 
-lineplot(Z)
+    rcParams = PyPlot.PyDict(PyPlot.matplotlib."rcParams")
+    rcParams["pgf.preamble"]  = "\\usepackage{amsfonts}"
+
+    fig.patch.set_alpha(0)
+    ax.patch.set_alpha(0)
+    ax.tick_params(colors="white")
+
+    for loc in ["bottom", "top", "left", "right"]
+        ax.spines[loc].set_color("white")
+    end
+
+    return (fig, ax)
+end
+
+function plot_trajectories()
+
+    ls = [0.1, 0.5]
+    nrep = 3
+    ngrid = 200
+    xs = collect(range(0, 1, length = ngrid))
+
+    for (i, l) in enumerate(ls)
+        for kernel in [RBFKernel(l), OUKernel(l)]
+
+            Sigma = get_Sigma(xs, kernel)
+            Sigma_svd = svd(Sigma)
+            Zs = [sample_gaussian(Sigma_svd) for _ in 1:nrep]
+            fig, ax = plt.subplots(figsize=(6,4))
+            colors = ["#bd93f9", "#50fa7b", "#ffb86c", "#8be9fd", "#ff79c6"]
+
+            for (j, Z) in enumerate(Zs)
+                ax.plot(xs, Z, color = colors[j])
+            end
+
+            plt.xlabel("Index, \$x\$", color = "#FFFFFF", size=10)
+            plt.ylabel("Gaussian process, \$Z_x\$", color = "#FFFFFF", size=10, labelpad=12)
+            ax.set_yticks(-3:3)
+            fig, ax = format_plot(fig, ax)
+            savefig("trajectory_$(shortname(kernel))_$i.pgf")
+            close("all")
+        end
+    end
+end
+
+function plot_bounds()
+    # TODO
+end
+
+
+function main()
+    Random.seed!(314159)
+    plt.ioff()
+    plot_trajectories()
+    #plot_bounds()
+end
+
+main()
+
+#kernel = OUKernel(l)
+
+#lineplot(Z)
 
 #mean_Z = 0.0
 #nreps = 100
@@ -50,5 +108,5 @@ lineplot(Z)
     #global mean_Z += Z / nreps
 #end
 
-#println(scale)
+#println(l)
 #println(mean_Z)
